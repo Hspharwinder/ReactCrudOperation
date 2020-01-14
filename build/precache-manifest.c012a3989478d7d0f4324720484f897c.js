5378d596ea419fb30055cd8e80bd51b4157bef72
HSP <40883035+Hspharwinder@users.noreply.github.com>
2020-01-14 10:10:12 +0530

    ReactCrud With JWT Auth

---
+++ build/precache-manifest.c012a3989478d7d0f4324720484f897c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83252a0467a971dfe455426fa7b0d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de97e6b04e31c44bd441",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9a5ceae4352bca24cec5",
+    "url": "/static/css/main.64322815.chunk.css"
+  },
+  {
+    "revision": "de97e6b04e31c44bd441",
+    "url": "/static/js/2.690cfb09.chunk.js"
+  },
+  {
+    "revision": "9a5ceae4352bca24cec5",
+    "url": "/static/js/main.b66e9060.chunk.js"
+  },
+  {
+    "revision": "a31027a2d7f20402e08f",
+    "url": "/static/js/runtime-main.e9a703a9.js"
+  }
+]);
